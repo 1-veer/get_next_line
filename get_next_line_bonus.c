66b58fd0f3a2253745a2cddb6dc72ff29928644f
Ayoub Bougatia <abougati@c3r5p6.1337.ma>
2024-11-23 17:30:37 +0100

    done

---
+++ get_next_line_bonus.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abougati <abougati@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/23 16:59:13 by abougati          #+#    #+#             */
+/*   Updated: 2024/11/23 17:27:40 by abougati         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer[MAX_FD];
+	char		*my_line;
+
+	if (fd < 0 || fd >= MAX_FD || BUFFER_SIZE <= 0 || BUFFER_SIZE >= 2147483647)
+		return (helper(&buffer[fd]));
+	buffer[fd] = read_and_add(fd, buffer[fd]);
+	if (!buffer[fd])
+		return (helper(&buffer[fd]));
+	my_line = extract_line(buffer[fd]);
+	buffer[fd] = adjust_buffer(buffer[fd]);
+	return (my_line);
+}
+
+char	*read_and_add(int fd, char *buffer)
+{
+	int		bytes_read;
+	char	*read_buff;
+
+	read_buff = (char *)malloc(BUFFER_SIZE + 1);
+	if (!read_buff)
+		return (NULL);
+	bytes_read = 1;
+	while (!check_for_endline(buffer) && bytes_read > 0)
+	{
+		bytes_read = read(fd, read_buff, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(read_buff);
+			return (NULL);
+		}
+		if (bytes_read == 0)
+			break ;
+		read_buff[bytes_read] = '\0';
+		buffer = ft_join(buffer, read_buff);
+	}
+	free(read_buff);
+	return (buffer);
+}
+
+char	*extract_line(char *buffer)
+{
+	char	*line;
+	int		i;
+
+	if (!buffer || !*buffer)
+		return (NULL);
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\n')
+		i++;
+	line = (char *)malloc(i + 1);
+	if (!line)
+		return (NULL);
+	line = copy(buffer, line, i);
+	return (line);
+}
+
+char	*adjust_buffer(char *buffer)
+{
+	char	*whats_left;
+	int		i;
+	int		j;
+
+	if (!buffer || !*buffer)
+		return (helper(&buffer));
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\n')
+		i++;
+	j = 0;
+	while (buffer[i + j])
+		j++;
+	if (j == 0)
+		return (helper(&buffer));
+	whats_left = (char *)malloc(j + 1);
+	if (!whats_left)
+		return (helper(&buffer));
+	whats_left = copy(buffer + i, whats_left, j);
+	free(buffer);
+	buffer = NULL;
+	return (whats_left);
+}
+
+char	*helper(char **buffer)
+{
+	if (*buffer)
+	{
+		free(*buffer);
+		*buffer = NULL;
+	}
+	return (NULL);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abougati <abougati@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/23 16:59:31 by abougati          #+#    #+#             */
+/*   Updated: 2024/11/23 17:12:29 by abougati         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 256
+# endif
+
+# define MAX_FD 1024
+
+char	*get_next_line(int fd);
+char	*read_and_add(int fd, char *buffer);
+char	*extract_line(char *buffer);
+char	*adjust_buffer(char *buffer);
+char	*helper(char **buffer);
+
+int		check_for_endline(char *buff);
+char	*copy(char *buffer, char *line, int i);
+char	*ft_join(char *s1, char *s2);
+char	*dupp(char *s);
+int		get_len(char *s);
+
+#endif
